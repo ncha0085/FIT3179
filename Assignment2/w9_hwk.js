3020728929775a45b6f590015da45cebff18e0a5
Nicholas Chan <ncha0085@student.monash.edu>
2025-09-27 23:35:38 +1000

    debugging

--- Assignment2/w9_hwk.js
+++ Assignment2/w9_hwk.js
@@ -1,4 +1,4 @@
-var vg_1 = "flight_path_map.vg.json";
+var vg_1 = "Assignment2/json/flight_path_map.vg.json";
 
 vegaEmbed("#flight_map", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
